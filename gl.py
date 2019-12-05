1c2d8046f37bfb65642c63fa2138c6f9da7109f8
amarG98 <57706183+amarG98@users.noreply.github.com>
2019-12-05 16:02:48 +0100

    programme V4 TOUT MARCHE

--- gl.py
+++ gl.py
@@ -1,9 +1,9 @@
-
-import sys
-import os
-import os.path
-import shutil
-
+import sys      #argument
+import os       #commande de base
+import os.path  #le path du program
+import shutil   #pour suprimmer récursivement
+import xml.etree.ElementTree as xmlElement #XML
+from lxml import etree #XML
 
 def Resume(Content):
     if "ABSTRACT" in Content :                                      # identifier le début du résumé
@@ -27,11 +27,11 @@ def Resume(Content):
         a2 = a1[0].split("\n")
         a = ''.join(a2)
         return a
-
+ # identifier l'auteur
 def Auteur(Content,titre):
 	titre2 = titre.split("Abstract",1)
 	return titre2[0]
-
+ # identifier les titres
 def Titre(Content):
     title = Content.split("\n",1)
     titre = title[0]
@@ -41,7 +41,153 @@ def Titre2(Content):
 	title = Content.split("\n",1)
 	titre = title[1]
 	return titre
+     # identifier la bib
 
+def Bibliographie(normalContent):
+    lowerContent = normalContent.lower()
+    if " references" in lowerContent:
+        indexFound = lowerContent.find("\nreferences\n")
+    else:
+        indexFound = lowerContent.find("references\n")
+    if indexFound == -1 :
+        return "bibliography not found"
+    indexFound += len("references\n")
+    normalContent = normalContent[indexFound:]
+    indexFound = normalContent.find("\n\n");
+    if indexFound == -1 :
+        return "bibliography not found"
+
+    if "[1]" in normalContent:
+        contentOnLine = normalContent.replace(".\n","  ;  ").replace("\n"," ")
+        contentOnLine = contentOnLine.replace(";", "\n")
+        return contentOnLine
+    elif "(2013)" in normalContent:
+        normalContent = normalContent.replace("\n"," ")
+        for i in range(1900,2020):
+            if str(i) in normalContent:
+                a = "("+str(i)+")"
+                b = "("+str(i)+")\n"
+
+                normalContent = normalContent.replace(a, b)
+        return normalContent
+    else:
+        normalContent = normalContent.replace("\n"," ")
+        for i in range(1900,2020):
+            if str(i) in normalContent:
+                a = str(i)+"."
+                b = str(i)+".\n"
+
+
+                normalContent = normalContent.replace(a, b)
+        return normalContent
+     # identifier l'introduction'
+
+def Intro(Content):
+	if "Introduction" in Content :
+		debut = Content.split("Introduction\n",1)
+		if "Mikolov" in Content:
+			fin = "2 "
+		else:
+			fin = "2\n"
+		a=debut[1].split(fin)
+		a1=a[0]
+		a = ''.join(a1)
+
+		return a
+
+	elif "INTRODUCTION" :
+		debut = Content.split("INTRODUCTION\n",1)
+		if "Naive" in Content:
+			fin = "2."
+		else :
+			fin = "2\n"
+		a=debut[1].split(fin)
+		a1=a[0]
+		a = ''.join(a1)
+
+		return a
+
+     # identifier la conclusion
+
+def Conclusion(Content):
+	if "Conclusion" in Content:
+		debut = Content.split("Conclusion",1)
+		if "Acknowledgments" in Content:
+			fin= "Acknowledgments"
+		elif "Acknowledgements" in Content:
+			fin= "Acknowledgements"
+		else:
+			fin= "References\n"
+
+		a = debut[1].split(fin)
+		a1 = a[0].split("\n")
+		a = ''.join(a1)
+		return a
+	elif "CONCLUSION" :
+
+		debut = Content.split("CONCLUSION",1)
+		if "ACKNOWLEDGMENT" in Content:
+			fin= "ACKNOWLEDGMENT"
+		else:
+			fin= "REFERENCES\n"
+
+		a = debut[1].split(fin)
+		a1 = a[0].split("\n")
+		a = ''.join(a1)
+		return a
+	else:
+		return " "
+     # identifier le corp
+
+def Corpse(Content):
+	if "Introduction" in Content:
+		debut = Content.split("Introduction",1)
+
+		if "Acknowledgments" in Content:
+			fin = "Acknowledgments"
+		else:
+			fin = "References"
+		a = debut[1].split(fin)
+		a1 = a[0].split("\n")
+		a = ''.join(a1)
+		return a
+	elif "INTRODUCTION" in Content:
+		debut = Content.split("INTRODUCTION",1)
+		if "Acknowledgments" in Content:
+			fin = "Acknowledgments"
+		else:
+			fin = "References"
+		a = debut[1].split(fin)
+		a1 = a[0].split("\n")
+		a = ''.join(a1)
+		return a
+
+     # identifier la disccussion
+
+def Discuss(Content):
+	if "Acknowledgments" in Content:
+		debut = Content.split("Acknowledgments",1)
+		fin="References"
+		a = debut[1].split(fin)
+		a1 = a[0].split("\n")
+		a = ''.join(a1)
+		return a
+	elif "Acknowledgements" in Content:
+		debut = Content.split("Acknowledgements",1)
+		fin="References"
+		a = debut[1].split(fin)
+		a1 = a[0].split("\n")
+		a = ''.join(a1)
+		return a
+	elif "ACKNOWLEDGMENT" in Content:
+		debut = Content.split("ACKNOWLEDGMENT",1)
+		fin="REFERENCES"
+		a = debut[1].split(fin)
+		a1 = a[0].split("\n")
+		a = ''.join(a1)
+		return a
+	else :
+		return " "
 
 
 
@@ -52,6 +198,7 @@ def filtre(src,dst,element):
         dst.write("Nom du fichier d’origine : "+titre+"\n")
         txt = src.read()
         titre2 = titre
+
     #******Titre******
         for i in range(2000,2019) :
             if str(i) in titre :
@@ -59,7 +206,7 @@ def filtre(src,dst,element):
         try:
             annee
         except NameError:
-            dst.write("titre not found")
+            dst.write("")
         else:
             titre2 = titre.split(annee)[1]
         if "Rouge" in titre :
@@ -75,11 +222,27 @@ def filtre(src,dst,element):
         	dst.write("\n\n\nTitre du papier  : "+  title[0] + "\n")
     # on ecrit le titre du pdf dans le fichier de destination (deuxieme ligne)
     #******Résumé******   lecture du fichier dans une variable string
+            #----------Auteurs------------
+
+        dst.write("\n"+"Auteur : "+Auteur(txt,titre2))
+
         a2 = Resume(txt)
         dst.write("\n\n\nAbstract de l’auteur : ")
         for i in range(0,len(a2)) :  # ecriture du résumé dans le fichier de destination sur une seule ligne (troisieme ligne)
             dst.write(a2[i])
 
+        #---------Intro--------------
+        dst.write("\n"+"Introduction : "+Intro(txt))
+        #---------Corps--------------
+        dst.write("\n"+"Corps : "+Corpse(txt))
+        #---------Conclusion--------------
+        dst.write("\n"+"Conclusion : "+Conclusion(txt))
+        #---------Discussion--------------
+        dst.write("\n"+"Discussion : "+Discuss(txt))
+        #----------Biblio-------------
+
+        dst.write("\n"+"Biblio : "+Bibliographie(txt))
+ # transfert vers le result
 
 def transfertToResult(arg):
     tmp = "{}/result".format(arg)
@@ -120,19 +283,62 @@ def pdf(directoryPath):
             # renommer le fichier
             os.rename("{0}/{1}".format(directoryPath,fileName), "{0}/{1}".format(directoryPath,newFileName))
             # créer la commande qui permet de faire la conversion
-            pdfToTextCommand = "pdf2txt {1}/{2} >{1}/tmp/{3}.txt".format(os.getcwd(),directoryPath, newFileName, fileNameWithouExt)
+            pdfToTextCommand = "pdftotext {1}/{2} {1}/tmp/{3}.txt".format(os.getcwd(),directoryPath, newFileName, fileNameWithouExt)
             # Executer la commande de conversion
             os.system(pdfToTextCommand)
 
 
+def createXmlFile(src,dst,title,rwt):
 
+    filename = dst+rwt+".xml"
+    titre = title.replace('.txt','').replace('_',' ')
+    txt = src.read()
+
+    root = xmlElement.Element("article")
+
+    element0 = xmlElement.SubElement(root,"preamble").text = titre
+
+    element1 = xmlElement.SubElement(root,"titre").text = Titre(txt)
+
+    element2 = xmlElement.SubElement(root,"auteur").text = Auteur(txt, Titre2(txt))
+
+    element3 = xmlElement.SubElement(root,"abstract").text = Resume(txt)
+
+    element4 = xmlElement.SubElement(root,"introduction").text = Intro(txt)
+
+    element5 = xmlElement.SubElement(root,"corps").text = Corpse(txt)
+
+    element6 = xmlElement.SubElement(root,"conclusion").text = Conclusion(txt)
+
+    element7 = xmlElement.SubElement(root,"discussion").text = Discuss(txt)
+
+    element8 = xmlElement.SubElement(root,"biblio").text = Bibliographie(txt)
+
+
+    tree= xmlElement.ElementTree(root)
+    tree.write(filename)
+
+def createFichierXml(arg) :
+    tmp = "{}/result".format(arg)
+    if os.path.exists(tmp):
+        shutil.rmtree(tmp)
+    os.mkdir(tmp)
+    t = "{}/tmp".format(arg)
+    for element in os.listdir(t):
+        if element.endswith('.txt'):
+            rwt = os.path.splitext(os.path.basename(element))[0]
+            a = "{0}/result/".format(arg)
+            s = "{0}/tmp/".format(arg)
+            source = open(s+element,"r")
+            createXmlFile(source,a,element,rwt)
+            source.close()
 
 
 print("*       PDF CONVERTER        *")
 # S'assurer que notre programme reçois le bon nombre d'argument
 # le premier argument est le nom de notre script python
 # le deuxième argument est le répértoire content l'ensemble des fichiers pdf à convertir.
-# le troisiéme argument est pour choisir le type de sortie soit txt
+# le troisiéme argument est pour choisir le type de sortie soit txt soit XML
 
 if len(sys.argv) == 3:
  # Récupérer le répértoire courant ( cwd : current working directory )
@@ -149,10 +355,21 @@ if len(sys.argv) == 3:
      print(bcolors.FAIL + "L'argument passé en paramètre n'est pas un dossier." + bcolors.ENDC)
      sys.exit(2)
  # Verifier si le type de sortie est égale a txt
- if sys.argv[2] == '-text':
+ if sys.argv[2] == '-t':
      # Début de la conversion
      print("Conversion pdf to txt")
      print ("Conversion des fichier du répértoire " + directory)
      pdf(directory)
      transfertToResult(directory)
      t = "{}/tmp".format(directory)
+     shutil.rmtree(t)
+
+#si en veux la conversion XML
+if sys.argv[2] == '-x':
+    # Début de la conversion
+    print("Conversion pdf to xml")
+    print ("Conversion des fichier du répértoire " + directory)
+    pdf(directory)
+    createFichierXml(directory)
+    t = "{}/tmp".format(directory)
+    shutil.rmtree(t)
